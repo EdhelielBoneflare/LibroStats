60b50200c62ea2509c88d60d3452bba578afe019
gruzdevaAS <gruzdeva.as@outlook.com>
2026-01-31 10:54:24 +0300

    initial setup lib base

---
+++ backend/LibBase/src/main/java/me/librostats/libbase/LibBaseApplication.java
@@ -0,0 +1,13 @@
+package me.librostats.libbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibBaseApplication.class, args);
+	}
+
+}
